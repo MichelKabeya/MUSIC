aca508f6c73c54b942e19554922781401b41db3d
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-04-17 10:51:35 +0200

    created variable to hold song object's attributes

---
+++ app/src/main/java/za/ac/iie/mymusicapp/song.kt
@@ -0,0 +1,8 @@
+package za.ac.iie.mymusicapp
+
+class song (songName: String, songArtist: String, songDuration: String){
+}     // created variables to holds song object's attributes
+      var name : String=""
+      var artist : String=""
+      var duration : String=""
+      var year : Int=0
