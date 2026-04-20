104512d14735c8e29ff8e281535c955e9769a04a
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-04-20 15:39:55 +0200

    changed the icon image

--- app/src/main/java/za/ac/iie/mymusicapp/MainActivity.kt
+++ app/src/main/java/za/ac/iie/mymusicapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package za.ac.iie.mymusicapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        var songShowTxt = findViewById<TextView>(R.id.songDisplay)
+        var songDisplay = " "
+        //accessing the song class to populate the song objects with specific attributes
+        var song1= song("Choosin' Texas", "Ella Langley", "3:33",(2020))
+        var song2= song("IceBerg", "John D", "3:00", (2016))
+        var song3= song("falling apart","Helene","2:47", (2029))
+        var song4= song("RageBait", "Lil Junior", "3:23", (2020))
+        // creating an array to hold the song objects
+        var songs = arrayOf<song>(song1,song2,song3,song4)
+        // for loop to loop through and display songs from the array
+        for (song in songs) {
+            songDisplay += "${song.DisplaySong()}\n\n"
+        }
+        songShowTxt.text =songDisplay
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/za/ac/iie/mymusicapp/song.kt
+++ app/src/main/java/za/ac/iie/mymusicapp/song.kt
@@ -1,8 +1,43 @@
 package za.ac.iie.mymusicapp
 
-class song (songName: String, songArtist: String, songDuration: String){
+import android.R.attr.duration
+import android.R.attr.name
+import android.util.Log
+
+class song (songName: String, songArtist: String, songDuration: String, 0, " "){
 }     // created variables to holds song object's attributes
-      var name : String=""
-      var artist : String=""
-      var duration : String=""
-      var year : Int=0
+      songName: String,
+      songArtist: String,
+      songYear: Int,
+      songDuration: String) {}
+      var Name : String=""
+      var Artist : String=""
+      var Year : Int=0
+      var Duration : String=""
+      constructor (songName: String, songArtist: String) this(songName, songArtist, 0, " ")
+
+// initialize vatiables
+init {
+      Name = songName
+      Artist = songArtist
+      Year = songYear
+      Duration = songDuration
+}
+
+fun Play () {
+      Log.v("song","$name playing now")
+}
+fun Pause(){
+      Log.v("Song", "$name paused now")
+}
+fun Stop() {
+      Log.v("Song", "$name stopped now")
+}
+fun Favourite(): Boolean {
+      Log.v("Song", "$name favourite now")
+      return true
+}
+fun displaySong(): String {
+      val artist = ""
+      return "$name - $artist ($duration)"
+}
